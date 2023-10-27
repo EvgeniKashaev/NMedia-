ba655da9cf6cfd0d367878d076e563ab6d9699c8
KashaevE <9081603360ken@gmail.com>
2023-10-27 23:19:11 +0300

    1.0

---
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.nmedia.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+    }
+}
